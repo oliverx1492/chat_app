5347114e643ca003b17f0acbcdc40669409e5c15
Oliver <oliverx1492@protonmail.com>
2024-09-16 16:23:13 +0200

    backend änderung 3

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'https://chat-app-l6fb.onrender.com'], // Erlaubter Ursprung
+        origin: ['http://localhost:5173', 'https://chat-app-l6fb.onrender.com'], // Erlaubter Ursprung
         methods: ['GET', 'POST'], // Erlaubte Methoden
     },
 });
